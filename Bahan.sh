346ee4fb8bd876d807a6a70789f148661eb5b045
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-26 17:00:03 +0800

    Create Bahan.sh

---
+++ Bahan.sh
@@ -0,0 +1,9 @@
+pkg install figlet
+pkg install Ruby
+pkg install lolcat
+gem install lolcat
+pkg install curl
+pkg install php
+clear
+figlet -f slant Suscses | lolcat
+echo "Selanjutnya Ketik sh RAí.sh
